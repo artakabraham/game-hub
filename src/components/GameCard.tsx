fe85c2156f3fe404f42e431d1ced07d353e8c0da
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2024-02-11 23:17:14 +0400

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Games } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
     game: Games
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
 
     return (
         <Card borderRadius={10} overflow="hidden">
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize={"2x1"}>{game.name}</Heading>
                 <HStack justifyContent='space-between'>

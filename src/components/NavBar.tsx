089a2b3d5b0ae414777cda0bf48624b90f4f355e
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2024-03-14 16:50:25 +0400

    Change home page url

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
     return (
         <HStack padding="10px">
-            <Link href='/'>
+            <Link href='/game-hub/'>
                 <Image objectFit='cover' alt="logo" src={logo} boxSize="60px" />
             </Link>
             <SearchInput onSearch={onSearch} />

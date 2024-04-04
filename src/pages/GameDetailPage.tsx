2d04db0653a4034358b03b4fbc1e37687f47be20
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2024-04-04 16:46:10 +0400

    Build game trailler

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,19 +3,21 @@ import useGame from "../hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
     const { slug } = useParams();
-    const { data: game, isLoading, error } = useGame(slug!);
+    const { data: games, isLoading, error } = useGame(slug!);
 
     if (isLoading) return <Spinner />;
-    if (error || !game) throw error;
+    if (error || !games) throw error;
 
     return (
         <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-            <GameAttributes game={game} />
+            <Heading>{games.name}</Heading>
+            <ExpandableText>{games.description_raw}</ExpandableText>
+            <GameAttributes game={games} />
+            <GameTrailer gameId={games.id}/>
         </>
     )
 }

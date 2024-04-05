8570c2bfa6b0cf8df3c32430376e1dd5c542f677
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2024-04-05 15:56:48 +0400

    Modify game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,10 @@
-import { Card, CardBody, HStack, Heading, Image, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Games } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
     game: Games
@@ -12,22 +13,18 @@ interface Props {
 const GameCard = ({ game }: Props) => {
 
     return (
-        <LinkBox>
-            <LinkOverlay href={'games/' + game.slug}>
-                <Card>
-                    <Image alt={game.name} src={getCroppedImageUrl(game.background_image)} />
-                    <CardBody>
-                        <HStack justifyContent='space-between' marginBottom={3}>
-                            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-                            <CriticScore score={game.metacritic} />
-                        </HStack>
-                        <Heading fontSize={"2x1"}>
-                            <Text>{game.name}</Text>
-                            <Emoji rating={game.rating_top} /></Heading>
-                    </CardBody>
-                </Card>
-            </LinkOverlay>
-        </LinkBox>
+        <Card>
+            <Image alt={game.name} src={getCroppedImageUrl(game.background_image)} />
+            <CardBody>
+                <HStack justifyContent='space-between' marginBottom={3}>
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
+                <Heading fontSize={"2x1"}>
+                    <Link to={'games/' + game.slug}>{game.name}</Link>
+                    <Emoji rating={game.rating_top} /></Heading>
+            </CardBody>
+        </Card >
     )
 }
 
